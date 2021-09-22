57ffee86beaa46f1bf33bbaa020c4b2026a0de77
Aldrian <himalosc@gmail.com>
2021-09-22 22:50:19 +0800

    pseudo working database implemented

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,15 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 _app = None
 
 def create_app():
     _app = app = Flask(__name__, instance_relative_config=True)
+    CORS(app, resources={
+        r'/document/*': {'origins': '*'},
+        r'/*': {'origins': '*'}
+    })
 
     try:
         os.mkdir(app.instance_path)
@@ -13,9 +18,15 @@ def create_app():
 
     @app.route('/')
     def index():
-        return '<h1>Hi! You are accessing my root! <a href="">Here is the link for the api!</a><h1>'
+        return '<h1>Hi! You are accessing my root! <a href="https://github.com/KnackHops/Document_API">Here is the link for the api!</a><h1>'
+
+    from . import document
+
+    for compo in (document,):
+        app.register_blueprint(compo.bp)
 
     return app
+
 
 # if __name__ == '__main__':
 #     port = int(os.environ.get('PORT', 5000))
